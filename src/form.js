561edd9a174058377d78889ffcaec1921f5581ff
Kaare Hoff Skovgaard <khs@secoya.dk>
2014-05-12 13:55:17 +0200

    Added eventHandlerContext option, to set the context of event handlers

--- src/form.js
+++ src/form.js
@@ -319,7 +319,7 @@ Shift.Form = React.createClass({
 	triggerEvent: function(name, args){
 		if(this.props.events != null){
 			if(typeof(this.props.events[name]) == 'function'){
-				return this.props.events[name].apply(undefined, args);
+				return this.props.events[name].apply(this.props.eventHandlerContext || undefined, args);
 			}
 		}
 		return null;
